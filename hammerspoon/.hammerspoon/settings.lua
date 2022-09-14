47dce0454e1837719659316cd05d398376854a84
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-09-14 14:55:11 +0200

    Add hammerspoon config to own package

---
+++ hammerspoon/.hammerspoon/settings.lua
@@ -0,0 +1,13 @@
+--== Global Settings ==--
+-- Disable all window animation
+hs.window.animationDuration = 0
+
+-- custom alert style
+hs.alert.defaultStyle.radius = 4
+hs.alert.defaultStyle.fillColor = { white = 0, alpha = 0.9 }
+hs.alert.defaultStyle.strokeColor = { white = 0, alpha = 0 }
+hs.alert.defaultStyle.fadeInDuration = 0
+hs.alert.defaultStyle.fadeOutDuration = 0
+hs.alert.defaultStyle.textFont  = "PragmataPro"
+hs.alert.defaultStyle.textSize = 14
+hs.alert.defaultStyle.textStyle = { paragraphStyle = {alignment = "center"} }
