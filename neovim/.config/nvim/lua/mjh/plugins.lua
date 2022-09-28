179237582015edb76841c6864bd98e12f0e72d8a
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-09-28 10:58:53 +0200

    Add illuminate plugin with default config

--- neovim/.config/nvim/lua/mjh/plugins.lua
+++ neovim/.config/nvim/lua/mjh/plugins.lua
@@ -8,13 +8,13 @@ vim.cmd([[
 
 local status_packer, packer = pcall(require, "packer")
 if not status_packer then
-	print("Packer is not installed")
-	return
+  print("Packer is not installed")
+  return
 end
 
 local status_impatient, impatient = pcall(require, "impatient")
 if not status_impatient then
-	return
+  return
 end
 
 -- Enable impatient profiling (only enable when wanting to view profiling info)
@@ -22,103 +22,106 @@ end
 
 -- Have packer use a popup window
 packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
 })
 
 packer.startup(function(use)
-	-- Packer
-	use({ "wbthomason/packer.nvim" })
+  -- Packer
+  use({ "wbthomason/packer.nvim" })
 
-	-- Often used requirements
-	use({
-		"kyazdani42/nvim-web-devicons",
-		"nvim-lua/plenary.nvim",
-	})
+  -- Often used requirements
+  use({
+    "kyazdani42/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
+  })
 
-	-- Impatient
-	use({ "lewis6991/impatient.nvim" })
+  -- Impatient
+  use({ "lewis6991/impatient.nvim" })
 
-	-- Colorschemes
-	use({
-		"projekt0n/github-nvim-theme",
-		"morhetz/gruvbox",
-		"rebelot/kanagawa.nvim",
-		"folke/tokyonight.nvim",
-	})
-	use({ "catppuccin/nvim", as = "catppuccin" })
+  -- Colorschemes
+  use({
+    "projekt0n/github-nvim-theme",
+    "morhetz/gruvbox",
+    "rebelot/kanagawa.nvim",
+    "folke/tokyonight.nvim",
+  })
+  use({ "catppuccin/nvim", as = "catppuccin" })
 
-	-- Nvim tree
-	use({
-		"kyazdani42/nvim-tree.lua",
-		tag = "nightly", -- optional, updated every week. (see issue #1193)
-	})
+  -- Nvim tree
+  use({
+    "kyazdani42/nvim-tree.lua",
+    tag = "nightly", -- optional, updated every week. (see issue #1193)
+  })
 
-	-- Telescope
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.x",
-	})
-	use({
-		"nvim-telescope/telescope-fzf-native.nvim",
-		run = "make",
-	})
+  -- Telescope
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.x",
+  })
+  use({
+    "nvim-telescope/telescope-fzf-native.nvim",
+    run = "make",
+  })
 
-	-- LSP's
-	use({
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip",
-		"rafamadriz/friendly-snippets",
-		"jose-elias-alvarez/null-ls.nvim",
-	})
+  -- LSP's
+  use({
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+    "jose-elias-alvarez/null-ls.nvim",
+  })
 
-	-- Bufferline
-	use({
-		"romgrk/barbar.nvim",
-	})
+  -- Bufferline
+  use({
+    "romgrk/barbar.nvim",
+  })
 
-	-- Statusline
-	use({
-		"nvim-lualine/lualine.nvim",
-	})
+  -- Statusline
+  use({
+    "nvim-lualine/lualine.nvim",
+  })
 
-	-- Treesitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-treesitter/nvim-treesitter-context",
-	})
+  -- Treesitter
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter-context",
+  })
 
-	-- Wakatime
-	use({ "wakatime/vim-wakatime" })
+  -- Wakatime
+  use({ "wakatime/vim-wakatime" })
 
-	-- Which key
-	use({ "folke/which-key.nvim" })
+  -- Which key
+  use({ "folke/which-key.nvim" })
 
-	-- Autopair
-	use({ "windwp/nvim-autopairs" })
+  -- Autopair
+  use({ "windwp/nvim-autopairs" })
 
-	-- Startup
-	use({ "goolord/alpha-nvim" })
+  -- Startup
+  use({ "goolord/alpha-nvim" })
 
-	-- Comment
-	use({ "numToStr/Comment.nvim" })
+  -- Comment
+  use({ "numToStr/Comment.nvim" })
 
-	-- Neogit
-	use({ "TimUntersberger/neogit" })
+  -- Neogit
+  use({ "TimUntersberger/neogit" })
 
-	-- Colorizer
-	use({ "NvChad/nvim-colorizer.lua" })
+  -- Colorizer
+  use({ "NvChad/nvim-colorizer.lua" })
 
-	-- Stabilize
-	use({ "luukvbaal/stabilize.nvim" })
+  -- Stabilize
+  use({ "luukvbaal/stabilize.nvim" })
+
+  -- Illuminate
+  use({ "RRethy/vim-illuminate" })
 end)
