535ecaa05a68ecd72462fc588a032ba48518c0e7
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-09-28 17:37:36 +0200

    Add some ascii art and use it in alpha

--- neovim/.config/nvim/after/plugin/alpha.lua
+++ neovim/.config/nvim/after/plugin/alpha.lua
@@ -1,34 +1,34 @@
 local status_alpha, alpha = pcall(require, "alpha")
 if not status_alpha then
-  return
+	return
 end
 
 local status_theme, theme = pcall(require, "alpha.themes.dashboard")
 if not status_theme then
-  return
+	return
 end
 
 local ascii = require("mjh.utils.ascii")
 
-theme.section.header.val = ascii.slanted
+theme.section.header.val = ascii.relief
 
 theme.section.buttons.val = {
-  theme.button("f", "  - Find file", ":Telescope find_files<CR>"),
-  theme.button("r", "  - Recent", ":Telescope oldfiles<CR>"),
-  theme.button("n", "  - Nvim Config", ":cd ~/.dotfiles/neovim/.config/nvim | e .<CR>"),
-  theme.button("t", "  - Kitty Config", ":cd ~/.dotfiles/kitty/.config/kitty | e .<CR>"),
-  theme.button("q", "  - Quit", ":qa<CR>"),
+	theme.button("f", "  - Find file", ":Telescope find_files<CR>"),
+	theme.button("r", "  - Recent", ":Telescope oldfiles<CR>"),
+	theme.button("n", "  - Nvim Config", ":cd ~/.dotfiles/neovim/.config/nvim | e .<CR>"),
+	theme.button("t", "  - Kitty Config", ":cd ~/.dotfiles/kitty/.config/kitty | e .<CR>"),
+	theme.button("q", "  - Quit", ":qa<CR>"),
 }
 theme.section.buttons.opts.spacing = 0
 
 theme.section.footer.val = require("alpha.fortune")
 theme.config.layout = {
-  { type = "padding", val = 8 },
-  theme.section.header,
-  { type = "padding", val = 4 },
-  theme.section.buttons,
-  { type = "padding", val = 4 },
-  theme.section.footer,
+	{ type = "padding", val = 4 },
+	theme.section.header,
+	{ type = "padding", val = 4 },
+	theme.section.buttons,
+	{ type = "padding", val = 4 },
+	theme.section.footer,
 }
 alpha.setup(theme.opts)
 
