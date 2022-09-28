bae75665675011df0b355766f4a27781ccc5ee87
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-09-28 10:44:24 +0200

    Telescope stuff

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -8,7 +8,32 @@ telescope.setup({
 		buffers = {
 			theme = "dropdown",
 			previewer = false,
+			initial_mode = "normal",
+			mappings = {
+				n = {
+					["<C-d>"] = "delete_buffer",
+				},
+			},
 		},
+	},
+	extensions = {
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+		},
+	},
+	vimgrep_arguments = {
+		"rg",
+		"--color=never",
+		"--no-heading",
+		"--with-filename",
+		"--line-number",
+		"--column",
+		"--smart-case",
+		"--hidden",
+		"--glob=!.git/",
 	},
 })
 
