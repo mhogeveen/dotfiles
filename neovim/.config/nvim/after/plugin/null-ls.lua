e75147c11a4b7dd5ceb3098cfddaa147e61a2136
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-10-13 16:45:32 +0200

    Formatting

--- neovim/.config/nvim/after/plugin/null-ls.lua
+++ neovim/.config/nvim/after/plugin/null-ls.lua
@@ -1,67 +1,65 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-  print("Null-ls is not installed")
-  return
+	print("Null-ls is not installed")
+	return
 end
 
 local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client)
-      return client.name ~= "tsserver"
-    end,
-    bufnr = bufnr,
-  }, {
-    async = true,
-  })
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name ~= "tsserver"
+		end,
+		bufnr = bufnr,
+	})
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-  sources = {
-    null_ls.builtins.diagnostics.eslint_d.with({
-      condition = function(utils)
-        return utils.root_has_file({
-          ".eslintrc",
-          ".eslintrc.js",
-          ".eslintrc.cjs",
-          ".eslintrc.yaml",
-          ".eslintrc.yml",
-          ".eslintrc.json",
-        })
-      end,
-    }),
-    null_ls.builtins.formatting.eslint_d.with({
-      condition = function(utils)
-        return utils.root_has_file({
-          ".eslintrc",
-          ".eslintrc.js",
-          ".eslintrc.cjs",
-          ".eslintrc.yaml",
-          ".eslintrc.yml",
-          ".eslintrc.json",
-        })
-      end,
-    }),
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.phpcsfixer.with({
-      extra_args = { "--using-cache=no" },
-    }),
-    null_ls.builtins.formatting.stylelint,
-    null_ls.builtins.formatting.rustfmt,
-    null_ls.builtins.formatting.trim_newlines,
-    null_ls.builtins.formatting.trim_whitespace,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          lsp_formatting(bufnr)
-        end,
-      })
-    end
-  end,
+	sources = {
+		null_ls.builtins.diagnostics.eslint_d.with({
+			condition = function(utils)
+				return utils.root_has_file({
+					".eslintrc",
+					".eslintrc.js",
+					".eslintrc.cjs",
+					".eslintrc.yaml",
+					".eslintrc.yml",
+					".eslintrc.json",
+				})
+			end,
+		}),
+		null_ls.builtins.formatting.eslint_d.with({
+			condition = function(utils)
+				return utils.root_has_file({
+					".eslintrc",
+					".eslintrc.js",
+					".eslintrc.cjs",
+					".eslintrc.yaml",
+					".eslintrc.yml",
+					".eslintrc.json",
+				})
+			end,
+		}),
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.phpcsfixer.with({
+			extra_args = { "--using-cache=no" },
+		}),
+		null_ls.builtins.formatting.stylelint,
+		null_ls.builtins.formatting.rustfmt,
+		null_ls.builtins.formatting.trim_newlines,
+		null_ls.builtins.formatting.trim_whitespace,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					lsp_formatting(bufnr)
+				end,
+			})
+		end
+	end,
 })
