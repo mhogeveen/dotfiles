ca3f6b3930e048e26e467dacf9b547d4890dafc0
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-10-18 15:08:12 +0200

    Add correct colors for modes

--- neovim/.config/nvim/after/plugin/modes.lua
+++ neovim/.config/nvim/after/plugin/modes.lua
@@ -1,31 +1,31 @@
 local status, modes = pcall(require, "modes")
 if not status then
-  print("Modes is not installed")
-  return
+	print("Modes is not installed")
+	return
 end
 
 modes.setup({
-  colors = {
-    copy = "#f5c359",
-    delete = "#c75c6a",
-    insert = "#a6e3a1",
-    visual = "#cba6f7",
-  },
+	colors = {
+		copy = "#f9e2af",
+		delete = "#f38ba8",
+		insert = "#a6e3a1",
+		visual = "#cba6f7",
+	},
 
-  -- Set opacity for cursorline and number background
-  line_opacity = 0.15,
+	-- Set opacity for cursorline and number background
+	line_opacity = 0.1,
 
-  -- Enable cursor highlights
-  set_cursor = true,
+	-- Enable cursor highlights
+	set_cursor = true,
 
-  -- Enable cursorline initially, and disable cursorline for inactive windows
-  -- or ignored filetypes
-  set_cursorline = true,
+	-- Enable cursorline initially, and disable cursorline for inactive windows
+	-- or ignored filetypes
+	set_cursorline = true,
 
-  -- Enable line number highlights to match cursorline
-  set_number = true,
+	-- Enable line number highlights to match cursorline
+	set_number = true,
 
-  -- Disable modes highlights in specified filetypes
-  -- Please PR commonly ignored filetypes
-  ignore_filetypes = { "NvimTree", "TelescopePrompt" },
+	-- Disable modes highlights in specified filetypes
+	-- Please PR commonly ignored filetypes
+	ignore_filetypes = { "NvimTree", "TelescopePrompt" },
 })
