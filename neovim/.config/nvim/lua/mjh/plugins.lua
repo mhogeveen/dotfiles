b2b6e866e1f6040270e927e885e1b9f343576c64
Maarten Hogeveen <mhogeveen94@gmail.com>
2022-11-08 16:53:52 +0100

    Auto install packer on new neovim installs

--- neovim/.config/nvim/lua/mjh/plugins.lua
+++ neovim/.config/nvim/lua/mjh/plugins.lua
@@ -1,3 +1,14 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
   augroup packer_user_config
@@ -138,4 +149,10 @@ packer.startup(function(use)
 
 	-- Fidget
 	use({ "j-hui/fidget.nvim" })
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if ensure_packer() then
+		packer.sync()
+	end
 end)
