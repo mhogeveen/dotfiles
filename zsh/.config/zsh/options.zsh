93d0e94a06833516172475a479ca393d4b2d9572
Maarten Hogeveen <mhogeveen94@gmail.com>
2022-11-18 15:27:07 +0100

    Remove highlighting pasted content (zsh)

--- zsh/.config/zsh/options.zsh
+++ zsh/.config/zsh/options.zsh
@@ -2,3 +2,6 @@
 setopt autocd       # Using the AUTOCD option, you can simply type the name of a directory, and it will become the current directory.
 setopt globdots     # GLOBDOTS lets files beginning with a . be matched without explicitly specifying the dot.
 setopt appendhistory
+
+# No highlight after pasting
+zle_highlight=('paste:none')
