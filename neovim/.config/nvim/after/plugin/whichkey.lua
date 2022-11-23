f73916ae8f580da43058f40bd24eabc9266d95c7
Maarten Hogeveen <mhogeveen94@gmail.com>
2022-11-23 21:43:43 +0100

    Turn off whichkey for neovim builtins

--- neovim/.config/nvim/after/plugin/whichkey.lua
+++ neovim/.config/nvim/after/plugin/whichkey.lua
@@ -5,6 +5,22 @@ if not status then
 end
 
 whichkey.setup({
+	plugins = {
+		marks = false, -- shows a list of your marks on ' and `
+		registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+		spelling = {
+			enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+		},
+		presets = {
+			operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+			motions = false, -- adds help for motions
+			text_objects = false, -- help for text objects triggered after entering an operator
+			windows = false, -- default bindings on <c-w>
+			nav = false, -- misc bindings to work with windows
+			z = false, -- bindings for folds, spelling and others prefixed with z
+			g = false, -- bindings for prefixed with g
+		},
+	},
 	window = {
 		border = "rounded", -- none, single, double, shadow
 	},
