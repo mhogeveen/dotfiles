00f6da450e9df28caa2b89911d48ee4eb6d7fc77
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-01-05 08:53:33 +0100

    Add treesitter-textobjects plugin

--- neovim/.config/nvim/after/plugin/treesitter.lua
+++ neovim/.config/nvim/after/plugin/treesitter.lua
@@ -1,14 +1,26 @@
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-  print("Treesitter is not installed")
-  return
+	print("Treesitter is not installed")
+	return
 end
 
 treesitter.setup({
-  auto_install = true,
-  highlight = { enable = true, additional_vim_regex_highlighting = true },
-  incremental_selection = { enable = true },
-  textobjects = { enable = true },
-  indent = { enable = true },
-  autopairs = { enable = true },
+	auto_install = true,
+	highlight = { enable = true, additional_vim_regex_highlighting = false },
+	incremental_selection = { enable = true },
+	indent = { enable = true },
+	autopairs = { enable = true },
+	textobjects = {
+		enable = true,
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+			},
+		},
+	},
 })
