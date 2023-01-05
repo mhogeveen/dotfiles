558c048473ac1599c619336495d7e4398f2a279b
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-01-05 09:37:17 +0100

    feat: refactor plugins.lua to use lazy.nvim instead of packer.nvim (#2)

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -1,53 +1,57 @@
 local status, telescope = pcall(require, "telescope")
 if not status then
-	print("Telescope is not installed")
-	return
+  print("Telescope is not installed")
+  return
 end
 
 telescope.setup({
-	defaults = {
-		mappings = {
-			n = {
-				["<C-d>"] = require("telescope.actions").delete_buffer,
-			},
-			i = {
-				["<C-d>"] = require("telescope.actions").delete_buffer,
-			},
-		},
-	},
-	pickers = {
-		buffers = {
-			theme = "dropdown",
-			previewer = false,
-			initial_mode = "normal",
-		},
-	},
-	extensions = {
-		["fzf"] = {
-			fuzzy = true, -- false will only do exact matching
-			override_generic_sorter = true, -- override the generic sorter
-			override_file_sorter = true, -- override the file sorter
-			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-		},
-		["ui-select"] = {
-			require("telescope.themes").get_dropdown({
-				previewer = false,
-				initial_mode = "normal",
-			}),
-		},
-	},
-	vimgrep_arguments = {
-		"rg",
-		"--color=never",
-		"--no-heading",
-		"--with-filename",
-		"--line-number",
-		"--column",
-		"--smart-case",
-		"--hidden",
-		"--glob=!.git/",
-	},
+  defaults = {
+    mappings = {
+      n = {
+        ["<C-d>"] = require("telescope.actions").delete_buffer,
+      },
+      i = {
+        ["<C-d>"] = require("telescope.actions").delete_buffer,
+      },
+    },
+  },
+  pickers = {
+    buffers = {
+      theme = "dropdown",
+      previewer = false,
+      initial_mode = "normal",
+    },
+  },
+  extensions = {
+    ["fzf"] = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+    },
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown({
+        previewer = false,
+        initial_mode = "normal",
+      }),
+    },
+  },
+  vimgrep_arguments = {
+    "rg",
+    "--color=never",
+    "--no-heading",
+    "--with-filename",
+    "--line-number",
+    "--column",
+    "--smart-case",
+    "--hidden",
+    "--glob=!.git/",
+  },
 })
 
-telescope.load_extension("fzf")
-telescope.load_extension("ui-select")
+pcall(function()
+  telescope.load_extension("fzf")
+end)
+pcall(function()
+  telescope.load_extension("ui-select")
+end)
--- neovim/.config/nvim/lua/mjh/plugins.lua
+++ neovim/.config/nvim/lua/mjh/plugins.lua
@@ -1,94 +1,79 @@
-local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
-local status_packer, packer = pcall(require, "packer")
+local status_packer, lazy = pcall(require, "lazy")
 if not status_packer then
-	print("Packer is not installed")
-	return
+  print("Packer is not installed")
+  return
 end
 
-local status_impatient, impatient = pcall(require, "impatient")
-if not status_impatient then
-	return
-end
-
--- Enable impatient profiling (only enable when wanting to view profiling info)
--- impatient.enable_profile()
-
--- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-})
-
-packer.startup(function(use)
-	use({
-		{ "wbthomason/packer.nvim" },
-		{ "kyazdani42/nvim-web-devicons" },
-		{ "nvim-lua/plenary.nvim" },
-		{ "lewis6991/impatient.nvim" },
-		{ "catppuccin/nvim", as = "catppuccin" },
-		{ "kyazdani42/nvim-tree.lua", tag = "nightly" },
-		{ "nvim-telescope/telescope.nvim", tag = "0.1.x" },
-		{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
-		{ "nvim-telescope/telescope-ui-select.nvim" },
-		{ "williamboman/mason.nvim" },
-		{ "williamboman/mason-lspconfig.nvim" },
-		{ "neovim/nvim-lspconfig" },
-		{ "jose-elias-alvarez/null-ls.nvim" },
-		{ "hrsh7th/nvim-cmp" },
-		{ "hrsh7th/cmp-buffer" },
-		{ "hrsh7th/cmp-path" },
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-nvim-lua" },
-		{ "L3MON4D3/LuaSnip" },
-		{ "saadparwaiz1/cmp_luasnip" },
-		{ "onsails/lspkind.nvim" },
-		{ "rafamadriz/friendly-snippets" },
-		{ "nvim-lualine/lualine.nvim" },
-		{ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
-		{ "nvim-treesitter/nvim-treesitter-context" },
-		{ "nvim-treesitter/nvim-treesitter-textobjects" },
-		{ "nvim-treesitter/playground" },
-		{ "wakatime/vim-wakatime" },
-		{ "folke/which-key.nvim" },
-		{ "windwp/nvim-autopairs" },
-		{ "goolord/alpha-nvim" },
-		{ "numToStr/Comment.nvim" },
-		{ "TimUntersberger/neogit" },
-		{ "NvChad/nvim-colorizer.lua" },
-		{ "luukvbaal/stabilize.nvim" },
-		{ "RRethy/vim-illuminate" },
-		{ "lewis6991/gitsigns.nvim" },
-		{ "j-hui/fidget.nvim" },
-		{ "karb94/neoscroll.nvim" },
-		{ "mvllow/modes.nvim", tag = "v0.2.0" },
-		{ "mhogeveen/reach.nvim" },
-		{ "famiu/bufdelete.nvim" },
-	})
-
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if ensure_packer() then
-		packer.sync()
-	end
-end)
+lazy.setup({
+  { "kyazdani42/nvim-web-devicons" },
+  { "nvim-lua/plenary.nvim" },
+  { "catppuccin/nvim", as = "catppuccin" },
+  { "kyazdani42/nvim-tree.lua", tag = "nightly" },
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "telescope-fzf-native.nvim", "telescope-ui-select.nvim" },
+    lazy = true,
+    cmd = "Telescope",
+  },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true },
+  { "nvim-telescope/telescope-ui-select.nvim", lazy = true },
+  { "neovim/nvim-lspconfig", dependencies = { "mason-lspconfig.nvim" }, lazy = true },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim", lazy = true },
+  { "jose-elias-alvarez/null-ls.nvim", lazy = true },
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      "cmp-buffer",
+      "cmp-path",
+      "cmp-nvim-lsp",
+      "cmp-nvim-lua",
+      "cmp_luasnip",
+    },
+  },
+  { "hrsh7th/cmp-buffer", lazy = true },
+  { "hrsh7th/cmp-path", lazy = true },
+  { "hrsh7th/cmp-nvim-lsp", lazy = true },
+  { "hrsh7th/cmp-nvim-lua", lazy = true },
+  { "saadparwaiz1/cmp_luasnip", lazy = true },
+  { "L3MON4D3/LuaSnip", event = "InsertEnter", dependencies = {
+    "friendly-snippets",
+  } },
+  { "rafamadriz/friendly-snippets", lazy = true },
+  { "onsails/lspkind.nvim" },
+  { "nvim-lualine/lualine.nvim" },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-treesitter/nvim-treesitter-context" },
+  { "nvim-treesitter/nvim-treesitter-textobjects" },
+  { "nvim-treesitter/playground" },
+  { "wakatime/vim-wakatime" },
+  { "folke/which-key.nvim", event = "VeryLazy" },
+  { "windwp/nvim-autopairs", event = "InsertEnter" },
+  { "goolord/alpha-nvim" },
+  { "numToStr/Comment.nvim" },
+  { "TimUntersberger/neogit" },
+  { "NvChad/nvim-colorizer.lua" },
+  { "luukvbaal/stabilize.nvim" },
+  { "RRethy/vim-illuminate" },
+  { "lewis6991/gitsigns.nvim", event = "BufRead" },
+  { "j-hui/fidget.nvim" },
+  { "karb94/neoscroll.nvim" },
+  { "mvllow/modes.nvim", tag = "v0.2.0" },
+  { "mhogeveen/reach.nvim" },
+  { "famiu/bufdelete.nvim" },
+}, {})
