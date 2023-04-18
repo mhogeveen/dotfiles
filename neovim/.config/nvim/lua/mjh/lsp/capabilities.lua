7941f5119b54e624837bccd2a5ea24170126455c
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-04-18 19:03:13 +0200

    feat: refactor lsp setup

--- neovim/.config/nvim/lua/mjh/lsp/capabilities.lua
+++
@@ -1,9 +0,0 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not status_ok then
-	return
-end
-
-return cmp_nvim_lsp.default_capabilities(capabilities)
---
+++ neovim/.config/nvim/lua/mjh/lsp/ui.lua
@@ -0,0 +1,18 @@
+local ok, lspwindows = pcall(require, "lspconfig.ui.windows")
+if not ok then
+  print("'lspconfig.ui.windows' not found: 'nvim-lspconfig' is not installed")
+  return
+end
+
+-- Set window styles
+lspwindows.default_options.border = "single"
+
+-- Configure vim diagnostics
+vim.diagnostic.config({
+  virtual_text = false,
+  float = {
+    border = "single",
+  },
+})
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
