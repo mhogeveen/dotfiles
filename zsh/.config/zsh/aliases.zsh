6b5343da8b8cd831ec09a1a24f420418bb268cf7
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-04-23 08:59:52 +0200

    chore: remove unused aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -7,7 +7,7 @@ alias e='exit'
 alias grep='rg'
 alias :q='exit'
 alias ip='ipconfig getifaddr en0'
-alias up='brew upgrade && yarn global upgrade' # Upgrade global Packages
+alias up='brew upgrade && pnpm up --global --latest' # Upgrade global Packages
 alias localphp="${HOME}/Library/Application\ Support/Local/lightning-services/php-8.0.0+2/bin/darwin/bin/php"
 alias localcomposer="${HOME}/Library/Application\ Support/Local/lightning-services/php-8.0.0+2/bin/darwin/bin/php /Applications/Local.app/Contents/Resources/extraResources/bin/composer/composer.phar"
 alias ycu="ncu --packageManager yarn --format group --interactive"
@@ -15,15 +15,3 @@ alias tmux="tmux -f ${HOME}/.tmux.conf"
 alias rm="rm -i"
 alias mv="mv -i"
 alias cp="cp -i"
-
-# Wordpress related aliases
-alias clone:sb-starter="git clone git@bitbucket.org:socialbrothers/wordpress-starter-theme.git"
-alias clone:wp="git clone git@github.com:wordpress/wordpress"
-
-# Switch php versions
-alias "php7.4"="valet use php@7.4 && composer global update"
-alias "php8.0"="valet use php@8.0 && composer global update"
-alias "php8.1"="valet use php && composer global update"
-alias php74="valet use php@7.4 && composer global update"
-alias php8="valet use php@8.0 && composer global update"
-alias php81="valet use php && composer global update"
