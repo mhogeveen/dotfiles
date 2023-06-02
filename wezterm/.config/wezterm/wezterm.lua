e35dd3be73bddd82f22aeadb3bfe9222c487ea06
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-06-02 14:49:07 +0200

    feat(wez): add harfbuzz features

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 
 -- Font
-config.font = wezterm.font("BlexMono Nerd Font Mono")
+config.font = wezterm.font({ family = "BlexMono Nerd Font Mono", harfbuzz_features = { "zero=1" } })
 config.font_size = 13.5
 config.line_height = 1.2
 
