804ed0875132d87c39e261d382d61773f27a9f97
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-07-20 14:24:13 +0200

    feat(nvim): update stylelint lsp stuff

--- neovim/.config/nvim/after/plugin/null-ls.lua
+++ neovim/.config/nvim/after/plugin/null-ls.lua
@@ -4,15 +4,19 @@ if not status then
   return
 end
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.diagnostics.stylelint,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.phpcsfixer.with({
       extra_args = { "--using-cache=no" },
     }),
     null_ls.builtins.formatting.rustfmt,
     null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.stylelint,
     null_ls.builtins.formatting.trim_newlines.with({
       disabled_filetypes = { "md" },
     }),
@@ -21,14 +25,19 @@ null_ls.setup({
     }),
   },
   on_attach = function(client, bufnr)
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format()
+          vim.lsp.buf.format({
+            async = false,
+            bufnr = bufnr,
+            filter = function()
+              return client.name == "null-ls"
+            end,
+          })
         end,
       })
     end
