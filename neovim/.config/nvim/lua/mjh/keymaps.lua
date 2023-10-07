4ebcab900e32eced6744b15e84b2fd4782d2f8c7
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-10-07 14:17:43 +0200

    chore(nvim): formatting

--- neovim/.config/nvim/lua/mjh/keymaps.lua
+++ neovim/.config/nvim/lua/mjh/keymaps.lua
@@ -1,42 +1,41 @@
 local map = function(mode, keymap, command)
-  vim.keymap.set(mode, keymap, command, {
-    noremap = true,
-    silent = true,
-  })
+	vim.keymap.set(mode, keymap, command, {
+		noremap = true,
+		silent = true,
+	})
 end
 
 --Remap space as leader key
-map("", "<Space>", "<Nop>")
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+map('', '<Space>', '<Nop>')
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- Normal Mode --
 -- Better window navigation
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-j>", "<C-w>j")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-l>", "<C-w>l")
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
 
 -- Vertical movement
-map("n", "<C-d>", "<C-d>zz")
-map("n", "<C-u>", "<C-u>zz")
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
 
 -- Resize with arrows
-map("n", "<A-Up>", ":resize +2<CR>")
-map("n", "<A-Down>", ":resize -2<CR>")
-map("n", "<A-Left>", ":vertical resize -2<CR>")
-map("n", "<A-Right>", ":vertical resize +2<CR>")
+map('n', '<A-Up>', ':resize +2<CR>')
+map('n', '<A-Down>', ':resize -2<CR>')
+map('n', '<A-Left>', ':vertical resize -2<CR>')
+map('n', '<A-Right>', ':vertical resize +2<CR>')
 
 -- Move lines up and down
-map("n", "<C-p>", ":m-2<CR>==")
-map("n", "<C-n>", ":m+<CR>==")
+map('n', '<C-p>', ':m-2<CR>==')
+map('n', '<C-n>', ':m+<CR>==')
 
 -- Delete without overwriting yank register
-map("n", "dd", '"_dd')
+map('n', 'dd', '"_dd')
 
 -- Filetree explorer
 -- look in the drex.lua file
-map("n", "<leader>e", ":Explore<CR>")
 
 -- Symbols outline
 ----map("n", "<leader>s", ":SymbolsOutline<CR>")
@@ -52,12 +51,12 @@ map("n", "<leader>e", ":Explore<CR>")
 
 -- Visual Mode --
 -- Move lines up and down
-map("v", "<C-p>", ":m '<-2<CR>gv")
-map("v", "<C-n>", ":m '>+1<CR>gv=gv")
+map('v', '<C-p>', ":m '<-2<CR>gv")
+map('v', '<C-n>', ":m '>+1<CR>gv=gv")
 
 -- Stay in indent mode
-map("v", "<", "<gv")
-map("v", ">", ">gv")
+map('v', '<', '<gv')
+map('v', '>', '>gv')
 
 -- Sort selection
-map("v", "<C-s>", ":'<,'>sort<CR>")
+map('v', '<C-s>', ":'<,'>sort<CR>")
