6c44cf7cceaef2ac574f85666db0e57d30e64f57
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-10-07 10:54:30 +0200

    feat(nvim): barebones config

---
+++ neovim/.config/nvim/lua/mjh/autocmds.lua
@@ -0,0 +1,11 @@
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("HighlightYank", {}),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 40,
+    })
+  end,
+})
