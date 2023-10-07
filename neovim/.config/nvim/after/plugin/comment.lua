2578edf829ed72ca8df064cf90a097134f1f8e24
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-10-07 10:53:22 +0200

    feat(nvim): delete existing config

--- neovim/.config/nvim/after/plugin/comment.lua
+++
@@ -1,7 +0,0 @@
-local status, comment = pcall(require, "Comment")
-if not status then
-  print("Comment is not installed")
-  return
-end
-
-comment.setup()
--- neovim/.config/nvim/after/plugin/neoscroll.lua
+++
@@ -1,7 +0,0 @@
-local status, neoscroll = pcall(require, "neoscroll")
-if not status then
-  print("Neoscroll is not installed")
-  return
-end
-
-neoscroll.setup()
