0c8b12dffccd2d8da28958b4adf0cee3b00c3247
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-11-18 11:59:23 +0100

    feat(nvim): use none-ls for now

--- neovim/.config/nvim/lua/mjh/plugins/formatter.lua
+++ neovim/.config/nvim/lua/mjh/plugins/formatter.lua
@@ -1,6 +1,54 @@
 return {
   {
+    'nvimtools/none-ls.nvim',
+    cmd = { 'NullLsInfo', 'NullLsLog' },
+    event = { 'BufReadPre', 'BufNewFile' },
+    init = function()
+      local null_ls = require 'null-ls'
+      local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
+      null_ls.setup {
+        sources = {
+          null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.diagnostics.stylelint,
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.phpcsfixer.with {
+            extra_args = { '--using-cache=no' },
+          },
+          null_ls.builtins.formatting.rustfmt,
+          null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.formatting.stylelint,
+          null_ls.builtins.formatting.trim_newlines.with {
+            disabled_filetypes = { 'md' },
+          },
+          null_ls.builtins.formatting.trim_whitespace.with {
+            disabled_filetypes = { 'md' },
+          },
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method 'textDocument/formatting' then
+            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format {
+                  async = false,
+                  bufnr = bufnr,
+                  filter = function()
+                    return client.name == 'null-ls'
+                  end,
+                }
+              end,
+            })
+          end
+        end,
+      }
+    end,
+  },
+  {
     'stevearc/conform.nvim',
+    enabled = false,
     cmd = { 'ConformInfo' },
     event = { 'BufReadPre', 'BufNewFile' },
     opts = {
