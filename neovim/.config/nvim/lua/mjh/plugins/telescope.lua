9a7b98591d204f5ab331d78df7246d5ed09bc5c3
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-12-29 13:49:53 +0100

    fix(nvim): load telescope on LspAttach event for ui-select

--- neovim/.config/nvim/lua/mjh/plugins/telescope.lua
+++ neovim/.config/nvim/lua/mjh/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
   cmd = 'Telescope',
+  event = 'LspAttach',
   dependencies = {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     { 'nvim-telescope/telescope-ui-select.nvim' },
@@ -31,10 +32,10 @@ return {
       },
       extensions = {
         ['fzf'] = {
-          fuzzy = true, -- false will only do exact matching
+          fuzzy = true,                   -- false will only do exact matching
           override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true, -- override the file sorter
-          case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+          override_file_sorter = true,    -- override the file sorter
+          case_mode = 'smart_case',       -- or "ignore_case" or "respect_case"
         },
         ['ui-select'] = {
           require('telescope.themes').get_dropdown {
