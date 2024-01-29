6844c1cf6734e441eefd3d8ee7f413a5b54a8366
Maarten Hogeveen <mhogeveen94@gmail.com>
2024-01-29 16:51:11 +0100

    feat(hs): clean up hammerspoon config

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,19 +1,1 @@
-require 'settings'
-local quake = require 'quake'
-local geo = require 'geo'
-
 hs.loadSpoon('ControlEscape'):start()
-
--- Set up quake apps
-quake.init({ 'alt' }, 'space', 'kitty', geo.full)
-quake.init({ 'alt', 'shift' }, 'space', 'kitty', geo.terminalBottom)
-quake.init({ 'alt' }, 's', 'Spotify', geo.centered)
-
--- Reload Hammerspoon config
-hs.hotkey.bind({ 'cmd', 'alt', 'ctrl' }, 'r', function()
-  hs.reload()
-  hs.alert.closeAll()
-  hs.alert.show '🔧 Config reloaded'
-end)
-
-hs.application.enableSpotlightForNameSearches(true)
