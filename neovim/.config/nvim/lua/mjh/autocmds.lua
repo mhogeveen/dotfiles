21e9736ff4366e5f9d4cb7400d3544e8fa378788
Maarten Hogeveen <mhogeveen94@gmail.com>
2024-02-26 15:17:51 +0100

    feat(nvim): cursorhold document highlight lsp method check

--- neovim/.config/nvim/lua/mjh/autocmds.lua
+++ neovim/.config/nvim/lua/mjh/autocmds.lua
@@ -14,14 +14,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('mjh-lsp-attach', { clear = true }),
   callback = function(event)
-    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-      buffer = event.buf,
-      callback = vim.lsp.buf.document_highlight,
-    })
+    local isMethodSupported = false
 
-    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-      buffer = event.buf,
-      callback = vim.lsp.buf.clear_references,
-    })
+    for _, client in ipairs(vim.lsp.get_active_clients { event.buf }) do
+      if client.supports_method 'textDocument/documentHighlight' then
+        isMethodSupported = true
+      end
+    end
+
+    if isMethodSupported then
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = event.buf,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        callback = vim.lsp.buf.clear_references,
+      })
+    end
   end,
 })
