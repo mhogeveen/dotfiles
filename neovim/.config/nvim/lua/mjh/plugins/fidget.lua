5f29e3c8c28b63cec7edfbe61dad1ec391b28563
Maarten Hogeveen <mhogeveen94@gmail.com>
2024-03-11 22:22:04 +0100

    chore(nvim): clean up fidget config

--- neovim/.config/nvim/lua/mjh/plugins/fidget.lua
+++ neovim/.config/nvim/lua/mjh/plugins/fidget.lua
@@ -2,9 +2,6 @@ return {
   --- https://github.com/j-hui/fidget.nvim
   'j-hui/fidget.nvim',
   opts = {
-    progress = {
-      ignore = { 'null-ls' },
-    },
     notification = {
       window = {
         winblend = 0,
