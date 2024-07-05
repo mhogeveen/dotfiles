364644b77bc35c42b905f9477396cb1f6476684d
Maarten Hogeveen <mhogeveen94@gmail.com>
2024-07-05 16:54:14 +0200

    refactor(nvim): clean up document highlight autocommand

--- neovim/.config/nvim/lua/mjh/autocmds.lua
+++ neovim/.config/nvim/lua/mjh/autocmds.lua
@@ -13,17 +13,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- and clear them on CursorMoved(I)
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('mjh-lsp-attach', { clear = true }),
-  callback = function(event)
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
+  callback = function(lsp_attach_event)
+    local client = vim.lsp.get_client_by_id(lsp_attach_event.data.client_id)
     if client and client.server_capabilities.documentHighlightProvider then
+      local highlight_group = vim.api.nvim_create_augroup('mjh-lsp-highlight', { clear = false })
       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-        buffer = event.buf,
+        group = highlight_group,
+        buffer = lsp_attach_event.buf,
         callback = vim.lsp.buf.document_highlight,
       })
 
       vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-        buffer = event.buf,
+        group = highlight_group,
+        buffer = lsp_attach_event.buf,
         callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('mjh-lsp-detach', { clear = true }),
+        callback = function(lsp_detach_event)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = 'mjh-lsp-highlight', buffer = lsp_detach_event.buf }
+        end,
       })
     end
   end,
