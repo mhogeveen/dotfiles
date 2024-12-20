3c133c7696df4cd848329ca9cee01f0166f0ddb8
Maarten Hogeveen <mhogeveen94@gmail.com>
2024-12-20 15:16:33 +0100

    feat(nvim): clean up options

--- neovim/.config/nvim/lua/mjh/options.lua
+++ neovim/.config/nvim/lua/mjh/options.lua
@@ -1,29 +1,29 @@
 local options = {
-  number = true, -- precede each line with its line number
-  cursorline = true, -- highlight the text line of the cursor
-  signcolumn = 'yes:1', -- always display signcolumn of fixed width 1
-  timeoutlen = 500, -- time in ms to wait for a mapped sequence to complete
-  updatetime = 250, -- cursorhold time in ms
-  mouse = 'a', -- enable mouse in all modes
-  ignorecase = true, -- ignore case in search patterns
-  smartcase = true, -- makes ignore case smarter in certain circumstances
-  wrapscan = true, -- searches wrap around the end of the file
-  swapfile = false, -- disable swapfiles
-  inccommand = 'split', -- ?? recommended everywhere but not sure what it does (:h not helping)
-  hlsearch = false, -- highlight previous search pattern matches
   breakindent = true, -- continue visual indentation of wrapped lines
-  wrap = false, -- do not wrap lines by defualt
   clipboard = 'unnamedplus', -- copying goes into OS clipboard as well
+  cursorline = true, -- highlight the text line of the cursor
+  hlsearch = false, -- highlight previous search pattern matches
+  ignorecase = true, -- ignore case in search patterns
+  inccommand = 'split', -- ?? recommended everywhere but not sure what it does (:h not helping)
+  list = true, -- enable list characters
+  listchars = 'tab:»·,multispace:··· ', -- set list characters
+  mouse = 'a', -- enable mouse in all modes
+  number = true, -- precede each line with its line number
   pumheight = 10, -- max number of items in pop-up menu
+  scrolloff = 8, -- scroll offset from top and bottom of file
+  shiftwidth = 2, -- number of spaces to use for each step of (auto)indent
+  sidescrolloff = 8, -- scroll offset from left and right of file
+  signcolumn = 'yes:1', -- always display signcolumn of fixed width 1
+  smartcase = true, -- makes ignore case smarter in certain circumstances
   splitbelow = true, -- vertical split is created below current buffer
   splitright = true, -- hoizontal split is created to the right of current buffer
-  scrolloff = 8, -- scroll offset from top and bottom of file
-  sidescrolloff = 8, -- scroll offset from left and right of file
+  swapfile = false, -- disable swapfiles
+  timeoutlen = 500, -- time in ms to wait for a mapped sequence to complete
   undofile = true, -- save undo history to a file
-  shiftwidth = 2, -- number of spaces to use for each step of (auto)indent
+  updatetime = 250, -- cursorhold time in ms
+  wrap = false, -- do not wrap lines by defualt
+  wrapscan = true, -- searches wrap around the end of the file
 }
-
-vim.opt.formatoptions:remove 'o' -- Don't have `o` add a comment
 
 for k, v in pairs(options) do
   vim.opt[k] = v
