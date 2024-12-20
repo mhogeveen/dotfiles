5a2a57e1dbc437055ba54fabc39445c4338be792
Maarten Hogeveen <mhogeveen94@gmail.com>
2024-12-20 15:16:56 +0100

    feat(nvim): turn off comment continuation

--- neovim/.config/nvim/lua/mjh/autocmds.lua
+++ neovim/.config/nvim/lua/mjh/autocmds.lua
@@ -1,11 +1,19 @@
 -- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('HighlightYank', { clear = true }),
+  group = vim.api.nvim_create_augroup('mjh-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank {
       higroup = 'LspReferenceText',
       timeout = 100,
     }
+  end,
+})
+
+-- Turn off automatic comment continuation
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = vim.api.nvim_create_augroup('mjh-formatoptions', { clear = true }),
+  callback = function()
+    vim.opt.formatoptions = vim.opt.formatoptions - { 'c', 'r', 'o' }
   end,
 })
 
