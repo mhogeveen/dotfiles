c5d64d50a042643aa89bd3a82046231f8ce12542
Maarten Hogeveen <mhogeveen94@gmail.com>
2025-07-19 17:03:52 +0200

    feat(nvim): update conform config

--- neovim/.config/nvim/lua/mjh/plugins/formatter.lua
+++ neovim/.config/nvim/lua/mjh/plugins/formatter.lua
@@ -5,51 +5,49 @@ return {
   enabled = true,
   cmd = { 'ConformInfo' },
   event = { 'BufWritePre' },
-  opts = function()
-    ---@module "conform"
-    ---@type conform.setupOpts
-    return {
-      formatters_by_ft = {
-        javascript = { 'prettierd', 'prettier', 'biome', stop_after_first = true },
-        typescript = { 'prettierd', 'prettier', 'biome', stop_after_first = true },
-        javascriptreact = { 'prettierd', 'prettier', 'biome', stop_after_first = true },
-        typescriptreact = { 'prettierd', 'prettier', 'biome', stop_after_first = true },
-        svelte = { 'prettierd', 'prettier', stop_after_first = true },
-        css = { 'stylelint', 'prettierd', 'prettier', stop_after_first = true },
-        scss = { 'stylelint', 'prettierd', 'prettier', stop_after_first = true },
-        sass = { 'stylelint', 'prettierd', 'prettier', stop_after_first = true },
-        less = { 'stylelint', 'prettierd', 'prettier', stop_after_first = true },
-        html = { 'prettierd', 'prettier', stop_after_first = true },
-        json = { 'prettierd', 'prettier', stop_after_first = true },
-        yaml = { 'prettierd', 'prettier', stop_after_first = true },
-        markdown = { 'prettierd', 'prettier', stop_after_first = true },
-        graphql = { 'prettierd', 'prettier', stop_after_first = true },
-        lua = { 'stylua' },
-        rust = { 'rustfmt' },
-        php = { 'php_cs_fixer' },
-        http = { 'kulala' },
-        rest = { 'kulala' },
-        terraform = { 'terraform_fmt' },
-        ['*'] = { 'trim_newlines', 'trim_whitespace' },
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {
+      javascript = { 'prettierd', 'prettier', 'biome', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', 'biome', stop_after_first = true },
+      javascriptreact = { 'prettierd', 'prettier', 'biome', stop_after_first = true },
+      typescriptreact = { 'prettierd', 'prettier', 'biome', stop_after_first = true },
+      svelte = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'stylelint', 'prettierd', 'prettier', stop_after_first = true },
+      scss = { 'stylelint', 'prettierd', 'prettier', stop_after_first = true },
+      sass = { 'stylelint', 'prettierd', 'prettier', stop_after_first = true },
+      less = { 'stylelint', 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'prettierd', 'prettier', stop_after_first = true },
+      json = { 'prettierd', 'prettier', stop_after_first = true },
+      yaml = { 'prettierd', 'prettier', stop_after_first = true },
+      markdown = { 'prettierd', 'prettier', stop_after_first = true },
+      graphql = { 'prettierd', 'prettier', stop_after_first = true },
+      lua = { 'stylua' },
+      rust = { 'rustfmt' },
+      php = { 'php_cs_fixer' },
+      http = { 'kulala' },
+      rest = { 'kulala' },
+      terraform = { 'terraform_fmt' },
+      ['*'] = { 'trim_newlines', 'trim_whitespace' },
+    },
+    format_on_save = {
+      lsp_fallback = true,
+      async = false,
+      timeout_ms = 1000,
+    },
+    formatters = {
+      --   stylelint = {
+      --     args = { '--fix', '--stdin', '--stdin-filename', '$FILENAME' },
+      --     command = require('conform.util').from_node_modules 'stylelint',
+      --   },
+      kulala = {
+        command = 'kulala-fmt',
+        args = { 'format', '$FILENAME' },
+        stdin = false,
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-      formatters = {
-        --   stylelint = {
-        --     args = { '--fix', '--stdin', '--stdin-filename', '$FILENAME' },
-        --     command = require('conform.util').from_node_modules 'stylelint',
-        --   },
-        kulala = {
-          command = 'kulala-fmt',
-          args = { 'format', '$FILENAME' },
-          stdin = false,
-        },
-      },
-    }
-  end,
+    },
+  },
   keys = {
     {
       'gf',
