e57518e9b5e1176d336187c3cb61f68d4e131f63
Maarten Hogeveen <mhogeveen94@gmail.com>
2026-05-01 16:12:51 +0200

    feat(nvim): migrate from nvim-treesitter to tree-sitter-manager

--- neovim/.config/nvim/lua/mjh/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/mjh/plugins/treesitter.lua
@@ -1,91 +1,156 @@
 ---@type LazySpec
 return {
-  --- https://github.com/nvim-treesitter/nvim-treesitter
-  'nvim-treesitter/nvim-treesitter',
-  branch = 'main',
-  build = ':TSUpdate',
-  cmd = {
-    'TSInstall',
-    'TSInstallFromGrammar',
-    'TSLog',
-    'TSUninstall',
-    'TSUpdate',
-  },
-  main = 'nvim-treesitter',
-  dependencies = {
-    {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-      branch = 'main',
+  {
+    'romus204/tree-sitter-manager.nvim',
+    dependencies = {}, -- tree-sitter CLI must be installed system-wide
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
+    end,
+    opts = {
+      ensure_installed = {
+        'bash',
+        'c',
+        'css',
+        'ecma',
+        'go',
+        'gomod',
+        'gosum',
+        'gowork',
+        'graphql',
+        'hcl',
+        'html',
+        'html_tags',
+        'javascript',
+        'json',
+        'jsx',
+        'latex',
+        'lua',
+        'luadoc',
+        'luap',
+        'markdown',
+        'markdown_inline',
+        'php',
+        'php_only',
+        'proto',
+        'python',
+        'query',
+        'regex',
+        'rust',
+        'scss',
+        'svelte',
+        'swift',
+        'terraform',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'vue',
+        'yaml',
+        'zig',
+        'zsh',
+      }, -- list of parsers to install at the start of a neovim session
+      border = 'single', -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+      auto_install = true, -- if enabled, install missing parsers when editing a new file
+      highlight = false, -- treesitter highlighting is enabled by default
+      languages = {}, -- override or add new parser sources
+      parser_dir = vim.fn.stdpath 'data' .. '/site/parser',
+      query_dir = vim.fn.stdpath 'data' .. '/site/queries',
     },
   },
-  init = function()
-    local ensure_installed = {
-      'bash',
-      'c',
-      'css',
-      'go',
-      'gomod',
-      'gosum',
-      'gowork',
-      'html',
-      'javascript',
-      'json',
-      'latex',
-      'lua',
-      'luadoc',
-      'luap',
-      'markdown',
-      'markdown_inline',
-      'php',
-      'proto',
-      'python',
-      'query',
-      'regex',
-      'rust',
-      'scss',
-      'svelte',
-      'swift',
-      'terraform',
-      'tsx',
-      'typescript',
-      'vim',
-      'vimdoc',
-      'vue',
-      'yaml',
-      'zig',
-    }
-    local already_installed = require('nvim-treesitter.config').get_installed()
-    local parsers_to_install = vim
-      .iter(ensure_installed)
-      :filter(function(parser)
-        return not vim.tbl_contains(already_installed, parser)
-      end)
-      :totable()
-    require('nvim-treesitter').install(parsers_to_install)
+  {
+    --- https://github.com/nvim-treesitter/nvim-treesitter
+    'nvim-treesitter/nvim-treesitter',
+    enabled = false,
+    branch = 'main',
+    build = ':TSUpdate',
+    cmd = {
+      'TSInstall',
+      'TSInstallFromGrammar',
+      'TSLog',
+      'TSUninstall',
+      'TSUpdate',
+    },
+    main = 'nvim-treesitter',
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        branch = 'main',
+      },
+    },
+    init = function()
+      local ensure_installed = {
+        'bash',
+        'c',
+        'css',
+        'go',
+        'gomod',
+        'gosum',
+        'gowork',
+        'graphql',
+        'html',
+        'javascript',
+        'json',
+        'latex',
+        'lua',
+        'luadoc',
+        'luap',
+        'markdown',
+        'markdown_inline',
+        'php',
+        'proto',
+        'python',
+        'query',
+        'regex',
+        'rust',
+        'scss',
+        'svelte',
+        'swift',
+        'terraform',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'vue',
+        'yaml',
+        'zig',
+      }
+      local already_installed = require('nvim-treesitter.config').get_installed()
+      local parsers_to_install = vim
+        .iter(ensure_installed)
+        :filter(function(parser)
+          return not vim.tbl_contains(already_installed, parser)
+        end)
+        :totable()
+      require('nvim-treesitter').install(parsers_to_install)
 
-    vim.api.nvim_create_autocmd('FileType', {
-      callback = function()
-        -- Enable treesitter highlighting and disable regex syntax
-        pcall(vim.treesitter.start)
-        -- Enable treesitter-based indentation
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-  end,
-  opts = {
-    auto_install = true,
-    incremental_selection = { enable = true },
-    autopairs = { enable = true },
-    textobjects = {
-      enable = true,
-      select = {
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          -- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+    opts = {
+      auto_install = true,
+      incremental_selection = { enable = true },
+      autopairs = { enable = true },
+      textobjects = {
         enable = true,
-        lookahead = true,
-        keymaps = {
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+          },
         },
       },
     },
