a1be2de293316cd5950a2f0e1dda0bb220b5ecfc
Maarten Hogeveen <mhogeveen94@gmail.com>
2026-07-14 16:23:50 +0200

    feat(nvim): update orgmode config

--- neovim/.config/nvim/lua/mjh/plugins/orgmode.lua
+++ neovim/.config/nvim/lua/mjh/plugins/orgmode.lua
@@ -1,3 +1,11 @@
+local orgfiles_base_dir = '~/git/orgfiles'
+local orgfiles_locs = {
+  base = orgfiles_base_dir,
+  personal = orgfiles_base_dir .. '/personal.org',
+  work = orgfiles_base_dir .. '/work.org',
+  inbox = orgfiles_base_dir .. '/inbox.org',
+}
+
 ---@type LazySpec
 return {
   --- https://github.com/chipsenkbeil/org-roam.nvim
@@ -17,21 +25,67 @@ return {
         local Menu = require 'org-modern.menu'
         -- Setup orgmode
         require('orgmode').setup {
-          org_agenda_files = '~/git/orgfiles/**/*',
-          org_default_notes_file = '~/git/orgfiles/inbox.org',
-          win_split_mode = 'float',
+          org_agenda_files = orgfiles_locs.base .. '/**/*',
+          org_default_notes_file = orgfiles_locs.inbox,
+          org_todo_keywords = { 'TODO(t)', 'INPROGRESS(i)', 'WAITING(w)', '|', 'DONE(d)' },
+          org_todo_repeat_to_state = 'TODO',
+          win_split_mode = { 'float', 0.8 },
           org_startup_folded = 'showeverything',
-          org_todo_keywords = { 'TODO', 'INPROGRESS', 'WAITING', '|', 'DONE' },
           org_todo_keyword_faces = {
             INPROGRESS = ':foreground #F9E2AF',
             WAITING = ':foreground #FAB387',
           },
+          org_hide_emphasis_markers = true,
+          org_ellipsis = '[ ... ]',
           org_log_into_drawer = 'LOGBOOK',
           org_startup_indented = true,
           org_adapt_indentation = true,
           org_indent_mode_turns_off_org_adapt_indentation = false,
           org_indent_mode_turns_on_hiding_stars = false,
-          org_hide_emphasis_markers = true,
+          org_id_link_to_org_use_id = true,
+          org_agenda_custom_commands = {
+            p = {
+              description = 'Personal Agenda',
+              types = {
+                {
+                  type = 'agenda',
+                  org_agenda_overriding_header = 'Personal Agenda',
+                  org_agenda_files = { orgfiles_locs.personal },
+                },
+              },
+            },
+            w = {
+              description = 'Work Agenda',
+              types = {
+                {
+                  type = 'agenda',
+                  org_agenda_overriding_header = 'Work Agenda',
+                  org_agenda_files = { orgfiles_locs.work },
+                },
+              },
+            },
+          },
+          org_capture_templates = {
+            t = {
+              description = 'Task',
+              subtemplates = {
+                i = {
+                  description = 'Inbox',
+                  template = '** TODO %?\n %u',
+                },
+                p = {
+                  description = 'Personal',
+                  template = '** TODO %?\n %u',
+                  target = orgfiles_locs.personal,
+                },
+                w = {
+                  description = 'Work',
+                  template = '** TODO %?\n %u',
+                  target = orgfiles_locs.work,
+                },
+              },
+            },
+          },
 
           ui = {
             menu = {
@@ -49,6 +103,6 @@ return {
     },
   },
   opts = {
-    directory = '~/git/orgfiles',
+    directory = orgfiles_locs.base,
   },
 }
